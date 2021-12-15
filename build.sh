a334af3ea57e9d87fb1f662ab7ffcc3c0a6c2c23
0x000000000000000 <2283206327@qq.com>
2021-12-15 14:16:06 +0800

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cargo build --target wasm32-unknown-unknown --release --package template && \
- ic-cdk-optimizer ./target/wasm32-unknown-unknown/release/template.wasm -o ./target/wasm32-unknown-unknown/release/template.wasm
+cargo build --target wasm32-unknown-unknown --release --package irc20 && \
+ ic-cdk-optimizer ./target/wasm32-unknown-unknown/release/irc20.wasm -o ./target/wasm32-unknown-unknown/release/irc20.wasm
 
 #  cargo build --target wasm32-unknown-unknown --package chain_cloud --release
